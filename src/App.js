bc4eed36c4b29f12e7c94fa968ed5f7f728740b5
vikas rana <vikasrana7770@gmail.com>
2022-10-30 09:19:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import { Routes, Route } from 'react-router-dom'
 import Coins from './components/Coins'
 import Coin from './routes/Coin'
 import Navbar from './components/Navbar'
-import Searchbar from './components/Searchbar'
+
+
 
 
 function App() {
 
   const [coins, setCoins] = useState([])
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1000&page=1&sparkline=false'
 
   useEffect(() => {
     axios.get(url).then((response) => {
       setCoins(response.data)
-      // console.log(response.data[0])
+      console.log(response.data[0])
     }).catch((error) => {
       console.log(error)
     })
@@ -25,7 +26,6 @@ function App() {
   return (
     <>
       <Navbar />
-      <Searchbar />
       <Routes>
         <Route path='/' element={<Coins coins={coins} />} />
         <Route path='/coin' element={<Coin />}>
--- src/components/Coins.js
+++ src/components/Coins.js
@@ -3,18 +3,20 @@ import CoinItem from './CoinItem'
 import Coin from '../routes/Coin'
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-//import Pagination from '@material-ui/lab/Pagination';
-
-
 import './Coins.css'
+import { Pagination } from '@mui/material';
 
 const Coins = (props) => {
+    
     const [page , setPage] = useState(1);
+    
+   
     return (
         <div className='container'>
             <div>
+      
                 <div className='heading'>
-                    <p>#</p>
+                    <p>Rank</p>
                     <p className='coin-name'>Coin</p>
                     <p>Price</p>
                     <p>24h</p>
@@ -26,20 +28,21 @@ const Coins = (props) => {
                     return (
                         <Link to={`/coin/${coins.id}`} element={<Coin />} key={coins.id}>
                             <CoinItem coins={coins} />
-                            
-  {/* <Pagination count ={(props.coins?.length/10).toFixed(0)}></Pagination> */}
                       
                         </Link>
                         
                     )
                 })}
              
-           
+             <Pagination style = {{display: "flex" , justifyContent:"center" , marginBottom:20}}
+            count={(props.coins?.length/10).toFixed(0)} 
+ color="secondary" onChange={(_, value) =>{setPage(value); window.scroll(0,100);}}
+/>
 
             </div>
-            
-{/* count ={(props.coins?.length/10).toFixed(0)} */}
-
+           
+         
+ 
     
               
         </div>
